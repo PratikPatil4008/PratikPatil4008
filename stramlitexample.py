ceee7cf935bc2c0c627902f1020e338f0b255727
PratikPatil4008 <96903066+PratikPatil4008@users.noreply.github.com>
2021-12-31 12:39:35 +0530

    Update stramlitexample.py

--- stramlitexample.py
+++ stramlitexample.py
@@ -1,140 +1,80 @@
-import numpy as np
-import pandas as pd
 import streamlit as st
-import plotly.graph_objects as go
-from sklearn.datasets import load_iris
+import pandas as pd
+import numpy as np
+import pickle
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
+
+st.write("""
+# Penguin Prediction App
+
+This app predicts the **Palmer Penguin** species!
+
+Data obtained from the [palmerpenguins library](https://github.com/allisonhorst/palmerpenguins) in R by Allison Horst.
+""")
+
+st.sidebar.header('User Input Features')
+
+st.sidebar.markdown("""
+[Example CSV input file](https://raw.githubusercontent.com/dataprofessor/data/master/penguins_example.csv)
+""")
+
+# Collects user input features into dataframe
+uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
+if uploaded_file is not None:
+    input_df = pd.read_csv(uploaded_file)
+else:
+    def user_input_features():
+        island = st.sidebar.selectbox('Island',('Biscoe','Dream','Torgersen'))
+        sex = st.sidebar.selectbox('Sex',('male','female'))
+        bill_length_mm = st.sidebar.slider('Bill length (mm)', 32.1,59.6,43.9)
+        bill_depth_mm = st.sidebar.slider('Bill depth (mm)', 13.1,21.5,17.2)
+        flipper_length_mm = st.sidebar.slider('Flipper length (mm)', 172.0,231.0,201.0)
+        body_mass_g = st.sidebar.slider('Body mass (g)', 2700.0,6300.0,4207.0)
+        data = {'island': island,
+                'bill_length_mm': bill_length_mm,
+                'bill_depth_mm': bill_depth_mm,
+                'flipper_length_mm': flipper_length_mm,
+                'body_mass_g': body_mass_g,
+                'sex': sex}
+        features = pd.DataFrame(data, index=[0])
+        return features
+    input_df = user_input_features()
+
+# Combines user input features with entire penguins dataset
+# This will be useful for the encoding phase
+penguins_raw = pd.read_csv('penguins_cleaned.csv')
+penguins = penguins_raw.drop(columns=['species'])
+df = pd.concat([input_df,penguins],axis=0)
+
+# Encoding of ordinal features
+# https://www.kaggle.com/pratik1120/penguin-dataset-eda-classification-and-clustering
+encode = ['sex','island']
+for col in encode:
+    dummy = pd.get_dummies(df[col], prefix=col)
+    df = pd.concat([df,dummy], axis=1)
+    del df[col]
+df = df[:1] # Selects only the first row (the user input data)
+
+# Displays the user input features
+st.subheader('User Input features')
+
+if uploaded_file is not None:
+    st.write(df)
+else:
+    st.write('Awaiting CSV file to be uploaded. Currently using example input parameters (shown below).')
+    st.write(df)
+
+# Reads in saved classification model
+load_clf = pickle.load(open('penguins_clf.pkl', 'rb'))
+
+# Apply model to make predictions
+prediction = load_clf.predict(df)
+prediction_proba = load_clf.predict_proba(df)
 
 
-iris_data = load_iris()
-# separate the data into features and target
-features = pd.DataFrame(
-    iris_data.data, columns=iris_data.feature_names
-)
-target = pd.Series(iris_data.target)
+st.subheader('Prediction')
+penguins_species = np.array(['Adelie','Chinstrap','Gentoo'])
+st.write(penguins_species[prediction])
 
-# split the data into train and test
-x_train, x_test, y_train, y_test = train_test_split(
-    features, target, test_size=0.2, stratify=target
-)
-
-
-class StreamlitApp:
-
-    def __init__(self):
-        self.model = RandomForestClassifier()
-
-    def train_data(self):
-        self.model.fit(x_train, y_train)
-        return self.model
-
-    def construct_sidebar(self):
-
-        cols = [col for col in features.columns]
-
-        st.sidebar.markdown(
-            '<p class="header-style">Iris Data Classification</p>',
-            unsafe_allow_html=True
-        )
-        sepal_length = st.sidebar.selectbox(
-            f"Select {cols[0]}",
-            sorted(features[cols[0]].unique())
-        )
-
-        sepal_width = st.sidebar.selectbox(
-            f"Select {cols[1]}",
-            sorted(features[cols[1]].unique())
-        )
-
-        petal_length = st.sidebar.selectbox(
-            f"Select {cols[2]}",
-            sorted(features[cols[2]].unique())
-        )
-
-        petal_width = st.sidebar.selectbox(
-            f"Select {cols[3]}",
-            sorted(features[cols[3]].unique())
-        )
-        values = [sepal_length, sepal_width, petal_length, petal_width]
-
-        return values
-
-    def plot_pie_chart(self, probabilities):
-        fig = go.Figure(
-            data=[go.Pie(
-                    labels=list(iris_data.target_names),
-                    values=probabilities[0]
-            )]
-        )
-        fig = fig.update_traces(
-            hoverinfo='label+percent',
-            textinfo='value',
-            textfont_size=15
-        )
-        return fig
-
-    def construct_app(self):
-
-        self.train_data()
-        values = self.construct_sidebar()
-
-        values_to_predict = np.array(values).reshape(1, -1)
-
-        prediction = self.model.predict(values_to_predict)
-        prediction_str = iris_data.target_names[prediction[0]]
-        probabilities = self.model.predict_proba(values_to_predict)
-
-        st.markdown(
-            """
-            <style>
-            .header-style {
-                font-size:25px;
-                font-family:sans-serif;
-            }
-            </style>
-            """,
-            unsafe_allow_html=True
-        )
-
-        st.markdown(
-            """
-            <style>
-            .font-style {
-                font-size:20px;
-                font-family:sans-serif;
-            }
-            </style>
-            """,
-            unsafe_allow_html=True
-        )
-        st.markdown(
-            '<p class="header-style"> Iris Data Predictions </p>',
-            unsafe_allow_html=True
-        )
-
-        column_1, column_2 = st.beta_columns(2)
-        column_1.markdown(
-            f'<p class="font-style" >Prediction </p>',
-            unsafe_allow_html=True
-        )
-        column_1.write(f"{prediction_str}")
-
-        column_2.markdown(
-            '<p class="font-style" >Probability </p>',
-            unsafe_allow_html=True
-        )
-        column_2.write(f"{probabilities[0][prediction[0]]}")
-
-        fig = self.plot_pie_chart(probabilities)
-        st.markdown(
-            '<p class="font-style" >Probability Distribution</p>',
-            unsafe_allow_html=True
-        )
-        st.plotly_chart(fig, use_container_width=True)
-
-        return self
-
-
-sa = StreamlitApp()
-sa.construct_app()
+st.subheader('Prediction Probability')
+st.write(prediction_proba)
